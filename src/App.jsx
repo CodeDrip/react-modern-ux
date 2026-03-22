abcbca79fe377fb2f1f39b3528641f7ea86cd5e9
“codebydhae” <dhaesha@gmail.com>
2026-03-21 21:06:30 -0400

    section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Navbar from "./layout/Navbar"
 import Hero from "./sections/Hero"
 import About from "./sections/About"
-// import Projects from "./sections/Projects"
+import Projects from "./sections/Projects"
 // import Experience from "./sections/Experience"
 // import Testimonials from "./sections/Testimonials"
 // import Contact from "./sections/Contact"
@@ -14,8 +14,8 @@ function App() {
         <Hero/>
         <About />
 
-        {/* <Projects />
-        <Experience />
+        <Projects />
+        {/* <Experience />
         <Testimonials />
         <Contact /> */}
 
